417e5ccf26da1f2fa57e193bf4ffc06c7e859e83
Richard Pimentel <richardpiment230@gmail.com>
2026-02-14 18:25:59 -0300

    ajuste finais feitos com sucesso

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// Portfolio is served as static HTML via public/index.html
-// This file exists only for React build compatibility
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.createRoot(root).render(<React.Fragment />);
-}
